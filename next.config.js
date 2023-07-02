2c463dbd062d0e23ab4bce004bddb71b7e6cddab
prakash <developerprakash@gmail.com>
2023-07-02 14:27:23 +0530

    c

--- next.config.js
+++ next.config.js
@@ -1,41 +1,10 @@
-const nextConfig = {
-    // Customizing the build output directory
-    distDir: 'build',
-  
-    // Adding custom routes
-    async rewrites() {
-      return [
-        {
-          source: '/old-page',
-          destination: '/new-page',
-        },
-      ];
-    },
-  
-    // Enabling TypeScript support
-    typescript: {
-      // Enable type-checking during development
-      ignoreBuildErrors: true,
-    },
-  
-    // Modifying webpack configuration
-    webpack(config, { dev, isServer }) {
-      // Add a new webpack plugin
-      if (dev && !isServer) {
-        config.plugins.push(new MyCustomPlugin());
-      }
-  
-      // Modify the webpack config
-      // ...
-  
-      return config;
-    },
-  
-    // Adding environment variables
-    env: {
-      MY_CUSTOM_VARIABLE: 'Hello, World!',
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+const path = require('path');
+
+module.exports = {
+  distDir: 'build',
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Add any custom webpack configurations here
+
+    return config;
+  },
+};
